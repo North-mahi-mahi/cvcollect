78b4c703e43b7be3fad689ce1efffc8a4baebe41
North-mahi-mahi <nicolas.kulka@mahi-mahi.fr>
2013-04-24 16:29:04 +0200

    coucou

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cvcollect::Application.config.secret_token = '1d3f846412c5460103c2b380baa1821161219ac17558dbf877b19920b9b8792d3565b3d8d33ec43641fe12396c2edb61f1f0a0673fff8d2445b8df2abb9ace70'
